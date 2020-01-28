7b0c789ac7fcceec27dbe10bbe838a410e09cc66
shusheng227 <3169739410@qq.com>
2020-01-28 16:26:48 +0800

    update

---
+++ 困难/最近的回文数.cpp
@@ -0,0 +1,46 @@
+/**
+ * 给定一个整数 n ，你需要找到与它最近的回文数（不包括自身）。
+ **/
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    string nearestPalindromic(string n) {
+        long len = n.size(), num = stol(n), res, minDiff = LONG_MAX;
+        unordered_set<long> s;
+        s.insert(pow(10, len) + 1);
+        s.insert(pow(10, len - 1) - 1);
+        long prefix = stol(n.substr(0, (len + 1) / 2));
+        for (long i = -1; i <= 1; ++i) 
+        {
+            string pre = to_string(prefix + i);
+            string str = pre + string(pre.rbegin() + (len%2), pre.rend());
+            s.insert(stol(str));
+        }
+        s.erase(num);
+        for (long a : s) 
+        {
+        	
+            long diff = abs(a - num);
+            if (diff < minDiff) {
+                minDiff = diff;
+                res = a;
+            } else if (diff == minDiff) {
+            	res = min(res, a);
+			}     
+			 
+        }
+        return to_string(res);
+    }
+};
+
+int main() {
+	string str = "100";
+	Solution s;
+	string result = s.nearestPalindromic(str);
+	cout << result << endl;
+}
